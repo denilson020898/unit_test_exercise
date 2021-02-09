7e629a3e8cce8c171a41a2462cbe7c4b80bfcd77
Denilson <denilson020898@gmail.com>
2021-02-09 18:00:33 +0700

    unit test, assert, require

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module unit_test_exercise
 
 go 1.15
+
+require github.com/stretchr/testify v1.7.0
--- quiz/hello_world_test.go
+++ quiz/hello_world_test.go
@@ -1,8 +1,42 @@
 package quiz_test
 
 import (
+	"fmt"
 	"testing"
+	"unit_test_exercise/quiz"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
+func TestHelloWorld_Assert(t *testing.T) {
+	result := quiz.HelloWorld("Budi")
+	assert.Equal(t, result, "Hello Budi", "they should be equal")
+	fmt.Println("hehe", "done")
+}
+
+func TestHelloWorld_Require(t *testing.T) {
+	result := quiz.HelloWorld("Budi")
+	require.Equal(t, result, "Hello Budi", "they should be equal")
+	fmt.Println("hehe", "no require")
+}
+
 func TestHelloWorld(t *testing.T) {
+	result := quiz.HelloWorld("Denilson")
+	if result != "Hello Denilson" {
+		// unit test failed
+		// panic("Result is not 'Hello Denilson'")
+		t.Error("Result must be 'Hello Denilson'")
+	}
+	fmt.Println("TestHelloWorld Done")
+}
+
+func TestHelloWorld_Another(t *testing.T) {
+	result := quiz.HelloWorld("Another")
+	if result != "Hello Another" {
+		// unit test failed
+		// panic("Result is not 'Hello Another'")
+		t.Fatal("Result must be 'Hello Another'")
+	}
+	fmt.Println("TestHelloWorldAnother")
 }
